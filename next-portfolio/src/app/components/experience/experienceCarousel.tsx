559253e1e9f10172fe13b0a5945ee35c2046a615
Alex Cristoffanini <acristoffanini@gmail.com>
2024-03-04 11:04:26 -0800

    carousel implementation

--- next-portfolio/src/app/components/experience/experienceCarousel.tsx
+++ next-portfolio/src/app/components/experience/experienceCarousel.tsx
@@ -1,18 +1,23 @@
+'use client'
 // import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
+import { Card, Paper, Typography } from '@mui/material';
+import Image from 'next/image';
 
 const experiences = [
     {
+        name: 'Microsoft',
         image: '/images/MicrosoftLogo.png',
         altText: 'Microsoft Logo',
         position: 'Full Stack Engineer',
         height: 60,
         width: 300,
-        description: 'Contract role at The Coalition. Working within the web team to provide a AAA web experience for Gears of War fans, as well as providing value to the studio by supporting internal development.'
+        description: 'Working within the web team to provide a AAA web experience for Gears of War fans, as well as providing value to the studio by supporting internal development.'
     },
     {
+        name: 'Espresso Dolce',
         image: '/images/EspressoDolceLogo.png',
         altText: 'Espresso Dolce Logo',
         position: 'Web Developer',
@@ -21,6 +26,7 @@ const experiences = [
         description: 'Worked on a robust mom & pop ecommerce site specializing in espresso machines. Heavily emphasizing SEO for products, and a great customer experience.'
     },
     {
+        name: 'ProAce',
         image: '/images/ProAceLogo.jpg',
         altText: 'Pro Ace Logo',
         position: 'Web Developer',
@@ -29,6 +35,7 @@ const experiences = [
         description: 'Worked on front-end and back-end systems. Strong focus on making a customer-facing website optimized for conversions and SEO.'
     },
     {
+        name: 'Spicebox Books',
         image: '/images/SpiceBoxLogo.png',
         altText: 'Spicebox Logo',
         position: 'Web Developer',
@@ -42,10 +49,46 @@ const experiences = [
 const ExperienceCarousel = () => {
     return (
         <section>
-            [Carousel]
-            <Carousel useKeyboardArrows={true}>
-                <div>Test1</div>
-                <div>Test2</div>
+            <Carousel 
+                useKeyboardArrows
+                centerMode
+                centerSlidePercentage={30}
+                swipeable
+                emulateTouch
+                // autoPlay
+                infiniteLoop
+                showStatus={false}
+                // interval={}
+            >
+                {experiences.map((e) => {
+                    return (
+                            <Paper 
+                                elevation={3} 
+                                key={e.name} 
+                                style={{
+                                    marginLeft: 50,
+                                    marginRight: 50,
+                                    marginBottom:35,
+                                    padding:5
+                                }}
+                            >
+                                <div 
+                                    style={{
+                                        width:'100%',
+                                        display: 'flex',
+                                        // flexDirection: 'column',
+                                        justifyContent: 'center'
+                                    }}
+                                >
+                                    <div style={{width: e.width}}>
+                                        <Image src={`${e.image}`} alt={e.name} width={e.width} height={e.height}/>
+                                    </div>
+                                </div>                            
+                                <Typography variant='h4'>{e.position}</Typography>
+                                <Typography variant='body2'>{e.description}</Typography>
+                            </Paper>
+                    )
+                })}
             </Carousel>
         </section>
     )
