b14a68c4fed69c34eca8b7bb49071b8b9fe9348e
Alex Cristoffanini <acristoffanini@gmail.com>
2024-04-07 23:35:10 -0700

    cleanup

--- next-portfolio/src/app/posts/[id]/page.tsx
+++ next-portfolio/src/app/posts/[id]/page.tsx
@@ -1,22 +1,5 @@
 // import PostLayout from '@/components/post-layout'
-import { getAllPostIds, getPostData } from '@/lib/posts'
-
-export async function generateStaticParams() {
-    return [{ id: '1' }, { id: '2' }]
-}
-
-// async function getPost(params) {
-//     const res = await fetch(`https://.../posts/${params.id}`)
-//     const post = await res.json()
-   
-//     return post
-// }
-   
-// export default async function Post({ params }) {
-// const post = await getPost(params)
-
-// return <PostLayout post={post} />
-// }
+import { getPostData } from '@/lib/posts'
 
 export default async function Post({ params }) {
     const post = await getPostData(params.id)
